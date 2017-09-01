e3b542fc7467c0e21c95e13a269b7e94030c5779
Berber <berberkrop@gmail.com>
2017-09-01 15:44:46 +0200

    readz to deploy

--- custom.js
+++ custom.js
@@ -1,28 +1,54 @@
 
 
 $(document).ready(function() {
-		$("#submitbutton").click(function() {
-				$("#budget").val()  
-		})
-		$(".cat1button").click(function() {
-    		var purchase = Number(prompt("Insert purchase amount ($)"))
-    		var spent = Number($(this).siblings(".spent").html())
-    		var budget = Number($(this).siblings(".budget").html())
-    		var percentage = left(purchase, spent, budget)
-    		console.log("spent " + spent)
-    		console.log("purchase" + purchase)
-    		console.log("budget" + budget)
-    		console.log(percentage)
-    		$(this).siblings(".percentage").html(percentage)
-    		$(this).siblings(".spent").html(spent + purchase)
-    		
 
-        })
+	var widthPercentage = 0.6		
+
+	$(".percentage").css("width", (widthPercentage * 100) + "%")
+	$(".category p").css("border-color", "green")
+
+
+	console.log(widthPercentage)
+
+	$("#submitbutton").click(function() {
+		$("#budget").val()  
+	})
+
+	$(".cat1button").click(function() {
+		
+		var purchase = Number(prompt("Insert purchase amount ($)"))
+		var spent = Number($(this).siblings(".spent").html())
+		var budget = Number($(this).siblings(".budget").html())
+		var percentage = left(purchase, spent, budget)
+
+		var progressPercentage = ((percentage / 100) * widthPercentage * 100)
+		var remainingPercentage = (widthPercentage * 100 - progressPercentage)
+
+		console.log (progressPercentage)
+		console.log (remainingPercentage)
+
+
+		$(this).siblings(".percentage").html(percentage + "%")
+		$(this).siblings(".percentage").css("width", progressPercentage + "%")
+
+		$(this).siblings(".remainingPercentage").html((remainingPercentage / widthPercentage) + "%")
+		$(this).siblings(".remainingPercentage").css("width", remainingPercentage + "%")
+		$(this).siblings(".remainingPercentage").css("visibility", "visible")
+
+		console.log (remainingPercentage)
+
+
+		$(this).siblings(".spent").html(spent + purchase)
+
+    })
+
+	// $(".percentage").css("width", (percentageLeft * widthPercentage * 100) + "%")
+	$(".percentage").css("background-color", "green")
 
 //         function left(purchase, spent, budget) {
 // 	return ((budget - (spent + purchase))/ budget ) * 100
 // }
-    })
+})
 
 
 function left(purchase, spent, budget) {
@@ -30,6 +56,6 @@ function left(purchase, spent, budget) {
 	var totalSpent = spent + purchase
 	var totalLeft = budget - totalSpent
 	var percentageLeft = (totalLeft / budget) * 100
-	percentageLeft = String(percentageLeft) + "%"
+	// percentageLeft = String(percentageLeft) + "%"
 	return percentageLeft
 }
